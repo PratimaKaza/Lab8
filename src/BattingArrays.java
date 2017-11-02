8cf16855b5b24e2c4f2894360d24617f37881163
Pratima Kaza <pratimakaza@Pratimas-MacBook-Pro.local>
2017-11-02 07:16:19 -0400

    first commit without gitignore

---
+++ src/BattingArrays.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/*****@author pratimakaza******/
+
+public class BattingArrays {
+
+	public static void main(String[] args) {
+		// Prompt user for number of at bats
+		// get input from user
+		// prompt user for bases earned
+		// enter bases earned
+		double [] bases = new double[4];
+		int[] atBats = {0,1,2,3,4};
+		int[] bases1 = {0,1,0,2,3};
+		
+		double [] atBat = new double[4];
+		int sum = 0;
+		int avg = 0;
+		// calculate batting avg
+		//double avg = calculateBatAvg(arry);
+		for (int i = 0; i < bases1.length; i++) {
+			if (!(bases1[i] == 0)) {
+				//System.out.println((bases1[i]));
+				int[] brain = new int[bases1[i]];
+				System.out.println(brain[i]);
+			}
+			//System.out.println(sum += bases1[]);
+		}
+		
+		// display batting avg
+		// calc slugging %
+		// display slugging %
+
+	}
+
+	private static double calculateBatAvg(double[] arry) {
+		if 
+		return 0;
+	}
+
+}
